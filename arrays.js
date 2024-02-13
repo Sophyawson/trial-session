48f24f682c4ae483ed14567418e572160dbdf471
Sophia <sophieyawson12@gmail.com>
2024-02-13 20:28:14 +0000

    arrays

---
+++ arrays.js
@@ -0,0 +1,10 @@
+//Arrays
+
+const nameOfStudentsInSummerClass = [
+    "Robert",
+    "John",
+    "Thomas",
+    "Smith",
+]
+
+console.log(nameOfStudentsInSummerClass)
--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+// Variable declarations
+//
+//
+//
+//
+//
+//
+
+
+
 const studentDetails = {
     name: "Nhyira",
     age : 19,
     school : "University of Ghana",
 
 };
+
+// String methods
+const nameOfAPerson = "Sophia Yawson";
+
+//length
+console.log("Length:", nameOfAPerson.length);
