ca2eab05d8aa151416bf6ee90a87dd9eb7751dac
Sophia <sophieyawson12@gmail.com>
2024-02-13 20:56:20 +0000

    conditions

--- arrays.js
+++ arrays.js
@@ -6,5 +6,22 @@ const nameOfStudentsInSummerClass = [
     "Thomas",
     "Smith",
 ]
+ 
+//Changes values in arrays
+//nameOfStudentsInSummerClass[0] = "Nhyira";
+//nameOfStudentsInSummerClass[2] = "Sophia";
 
-console.log(nameOfStudentsInSummerClass)
+//console.log(nameOfStudentsInSummerClass);
+
+// Array Methods
+//console.log(nameOfStudentsInSummerClass.toString());
+ const person = ["John", "Doe", 46]
+ console.log(person);
+// Properties of Array 
+console.log
+//const animals = new Array(
+  //  "cat",
+ //   "dog",
+ //   "sheep",
+
+//)
---
+++ condition.js
@@ -0,0 +1,11 @@
+const age = 18;
+const isFemales = true;
+const isMale = false;
+const weight = 90;
+
+
+if (age == 18){
+    console.log("You are 18");
+} else {
+    console.log("You are not 18");
+}
